f33bf6adf7bbe04fc106a16f8e619b2a067b4de1
berkaydagdeviren <76188403+berkaydagdeviren@users.noreply.github.com>
2024-10-14 00:01:12 +0300

    Update RetailTracking.js

--- src/components/RetailTracking.js
+++ src/components/RetailTracking.js
@@ -16,14 +16,7 @@ const RetailTracking = () => {
     fetchSales().catch(error => console.error("Fetching sales failed", error));
     const fetchCompaniesAndProducts = async () => {
       try {
-        //const companiesResponse = await axios.get('/api/companies');
-        const response = await fetch('/api/companies');
-
-        if (!response.ok) {
-        throw new Error('Network response was not ok');
-        }
-        const companiesResponse = await response.json();
-        console.log(companiesResponse)
+        const companiesResponse = await axios.get('https://apii-iviq.onrender.com/api/companies');
         setCompanies(companiesResponse.data.map(company => ({ label: company.name, value: company._id })));
         const productsResponse = await axios.get('/api/products');
         setProducts(productsResponse.data.map(product => ({ label: product.name, value: product._id, price: product.price })));
