1d3e0b967112310662b727d12bcadfa8573cc005
berkaydagdeviren <76188403+berkaydagdeviren@users.noreply.github.com>
2024-10-20 13:29:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -58,4 +58,10 @@ app.use('/api/import', importRoutes);
 app.use(importProductRoutes);
 
 // Server listens on the port defined in the .env file, or 3002 as a fallback
+const PORT = process.env.PORT || 4000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}).on('error', (error) => {
+  console.error('Error starting server:', error);
+});
